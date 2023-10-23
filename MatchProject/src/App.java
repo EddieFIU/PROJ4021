7ccff15037fc8d9dbee8163df3b9b0c55c700d92
Eddie Molieri <emolieri@vpsdev.com>
2023-10-23 17:50:29 -0400

    adding some commenting

--- MatchProject/src/App.java
+++ MatchProject/src/App.java
@@ -10,17 +10,21 @@ public class App{
     public static void main(String[] args){
     
           try {
-             
+             //creating login business object
             LoginAccount loggedInUser = new LoginAccount();
+            //calling method in business object to return our model
             Model.LoginUser user = loggedInUser.GetAccountByUserName("eddie");
             System.out.println(user.UserType + " is the user type we are reading");
 
+            //creating candidate business object
             Candidate candidateInfo = new Candidate(); 
+            //returning model of logged in user by id
             Model.Candidate loggedinUser = candidateInfo.GetCandidateByLoginID(user.ID);
-
             System.out.println(loggedinUser.FirstName + " is logged in");
             
+            //creating company business object
             Company co = new Company();
+            //calling method in business object to return company model
             Model.Company companyInfo = co.GetCompanyByLoginID(2);
 
             if (companyInfo.ID==0)
@@ -31,16 +35,18 @@ public class App{
                 System.out.println(companyInfo.Name  + " is logged in");
             }
             
-
+            //calling method of business object instantiated above to return all companies
             List<Model.Company> allCompanies = co.GetAllCompanies();
             for (Model.Company company : allCompanies) {
               System.out.println("Company is: " + company.Name);
               
+              //using the same business object to return the projects based on company currently in for loop
               List<Model.CompanyProject> projects = co.GetCompanyProjects(company.ID);
               for(Model.CompanyProject project : projects)
               {
                 System.out.println("project name is: " + project.ProjectName);
                 List<Model.ProjectRequirement> requirements = co.GetProjectRequirements(project.ID);
+                //calling same business object for company to return requirements by project.  all based on the company we are on in the for loop
                 for(Model.ProjectRequirement requirement:requirements)
                 {
                   System.out.println("requirement is: " + requirement.Requirement);
@@ -48,12 +54,14 @@ public class App{
                   
                 }
               }
-
             }
+
+            //calling candidate object already instantiated above to return all candiates
             List<Model.Candidate> allCandidates =  candidateInfo.GetAllCandidates();
             for (Model.Candidate candidate : allCandidates) {
                 System.out.println("Candidate is: " + candidate.FirstName);
-                List<Model.CandidateQualification> qualifications = candidateInfo.GetCandidateQualifications(candidate.ID);
+                //calling method to return the candidate's qualifications based on the current candidate ID in the for loop
+                List<Model.CandidateQualification> qualifications = candidateInfo.GetCandidateQualifications(candidate.ID);              
                 for(Model.CandidateQualification qualification : qualifications)
                 {
                   System.out.println("qualifications are: " + qualification.Qualification);
